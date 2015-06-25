eb9af0d8681b04b965a8f707f85bf56d5ae17312
Eric Wing <ewing -dot- public |-at-| gmail.com>
2015-06-25 06:10:09 -0700

    Added C files

---
+++ SwiftAndC/DoSomething.c
@@ -0,0 +1,14 @@
+//
+//  DoSomething.c
+//  SwiftAndC
+//
+//  Created by Eric Wing on 6/25/15.
+//  Copyright (c) 2015 PlayControl Software LLC. All rights reserved.
+//
+
+#include "DoSomething.h"
+
+int DoSomething()
+{
+	return 1;
+}
---
+++ SwiftAndC/DoSomething.h
@@ -0,0 +1,15 @@
+//
+//  DoSomething.h
+//  SwiftAndC
+//
+//  Created by Eric Wing on 6/25/15.
+//  Copyright (c) 2015 PlayControl Software LLC. All rights reserved.
+//
+
+#ifndef __SwiftAndC__DoSomething__
+#define __SwiftAndC__DoSomething__
+
+#include <stdio.h>
+int DoSomething(void);
+
+#endif /* defined(__SwiftAndC__DoSomething__) */
